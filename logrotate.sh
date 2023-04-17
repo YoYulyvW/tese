c57471ff96964cdfbd58cd8c1af5f30af210883a
YoYulyvW <62192576+YoYulyvW@users.noreply.github.com>
2023-04-17 19:31:22 +0800

    Update logrotate.sh

--- logrotate.sh
+++ logrotate.sh
@@ -29,3 +29,21 @@ systemctl restart rsyslog
 #chmod +x logrotate.sh
 #./logrotate.sh
 
+
+#设置要屏蔽的关键词
+keywords=("systemd-logind: New session" "Created slice" "Starting Session" "Started Session" "Wi-Fi" "hostapd")
+
+#将关键词转换为rsyslog配置格式
+rules=""
+for keyword in "${keywords[@]}"; do
+  rules+=":msg, contains, \"$keyword\" ~"$'\n'
+done
+
+#添加屏蔽规则到rsyslog配置文件
+sed -i "/# Log anything (except mail) of level info or higher./a $rules    daemon.*;mail.*;syslog;\
+        news.err;\
+        *.=debug;*.=info;\
+        *.=notice;*.=warn   /dev/null" /etc/rsyslog.conf
+
+#重启rsyslog服务
+systemctl restart rsyslog
