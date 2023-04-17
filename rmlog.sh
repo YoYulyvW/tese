e6a1e600e199de0ddcf68223f0ebe44c4c787ea5
YoYulyvW <62192576+YoYulyvW@users.noreply.github.com>
2023-04-17 20:17:14 +0800

    Update rmlog.sh

--- rmlog.sh
+++ rmlog.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# 检查当前系统是否是Armbian
-if [ $(grep -c "Armbian" /etc/os-release) -eq 0 ]; then
-    echo "该脚本仅适用于Armbian系统。"
-    exit 1
-fi
+# 添加crontab定时任务，每10分钟清空/var/log/daemon.log和/var/log/syslog文件
+echo "*/10 * * * * root cat /dev/null > /var/log/daemon.log && cat /dev/null > /var/log/syslog" >> /etc/crontab
 
-# 清空原始文件
-> /var/log/daemon.log
-> /var/log/syslog
-
-# 备份原始文件
-cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
-
-# 修改rsyslog.conf文件，屏蔽/var/log/daemon.log和/var/log/syslog文件的写入日志
-sed -i '/^daemon\.log/ s/^/#/' /etc/rsyslog.conf
-sed -i '/^syslog/ s/^/#/' /etc/rsyslog.conf
-
-# 重启rsyslog服务
-systemctl restart rsyslog
-
-echo "已修改/etc/rsyslog.conf文件，屏蔽了/var/log/daemon.log和/var/log/syslog文件的写入日志。"
+echo "已添加crontab定时任务，每10分钟清空/var/log/daemon.log和/var/log/syslog文件。"
